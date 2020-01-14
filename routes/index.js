7b4ed26e8b0e0db0dd91bcf7c21b041544f77f0e
Alison <alisonbessa@gmail.com>
2020-01-14 11:42:44 -0300

    fix page's title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('index');
+    let data = {
+        layout: false
+    }
+    res.render('index', data);
 });
 
 router.get('/dashboard', (req, res, next) => {
