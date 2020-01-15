b699cc59006536a251ef8983e1a0472003bcd884
Barbosa <reginaldobr@gmail.com>
2020-01-15 09:01:51 -0300

    node modules removed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
-<<<<<<< HEAD
 const express       = require('express');
 const hbs           = require('hbs');
 const app           = express();
@@ -9,14 +8,6 @@ const mongoose      = require('mongoose');
 const bodyParser    = require('body-parser');
 
 require("dotenv").config();
-=======
-const express = require('express');
-const hbs = require('hbs');
-const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-//const uploadCloud   = require('../config/cloudinary');
->>>>>>> bc154d3b22df7903f9d84776bc1a1619bde1a473
 
 mongoose
 .connect(process.env.MONGODB_URI, {
@@ -43,10 +34,4 @@ app.use('/auth', auth);
 const newReport = require('./routes/new-report');
 app.use('/new-report', newReport);
 
-<<<<<<< HEAD
-
-
-app.listen(process.env.PORT);
-=======
 app.listen(process.env.PORT, console.log(`Listening on port ${process.env.PORT}`));
->>>>>>> bc154d3b22df7903f9d84776bc1a1619bde1a473
