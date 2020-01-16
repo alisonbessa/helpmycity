ab89786af231a04d9365c918e77a6b26332feb40
Alison <alisonbessa@gmail.com>
2020-01-16 19:01:27 -0300

    separeting in 2 brenchs

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,7 +6,9 @@ const multer = require('multer');
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
+  api_secret: process.env.CLOUDINARY_SECRET,
+  exif: true,
+
 });
 
 var storage = cloudinaryStorage({
